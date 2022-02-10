cc7f8058e6e5c797506471edcf458e909a24d2a0
mrbob312 <34964788+mrbob312@users.noreply.github.com>
2022-02-10 23:12:03 +0100

    Made algorithm actually work

---
+++ ConsoleGUI.Drawing/Imaging/ConsolePixelColor.cs
@@ -0,0 +1,78 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ConsoleGUI.Drawing.Imaging
+{
+    internal class ConsolePixelColor
+    {
+        public static readonly int[] cColors = { 0x000000, 0x000080, 0x008000, 0x008080, 0x800000, 0x800080, 0x808000, 0xC0C0C0, 0x808080, 0x0000FF, 0x00FF00, 0x00FFFF, 0xFF0000, 0xFF00FF, 0xFFFF00, 0xFFFFFF };
+        public static readonly char[] ratios = new char[] { (char)0032, (char)9617, (char)9618, (char)9619 };
+        private static ConsolePixelColor[]? ConsolePixelColorCombosInternal;
+        public static ConsolePixelColor[] ConsolePixelColorCombos
+        {
+            get
+            {
+                if (ConsolePixelColorCombosInternal == null)
+                {
+                    ConsolePixelColorCombosInternal = GenerateColorCombos();
+                }
+                return ConsolePixelColorCombosInternal!;
+            }
+        }
+        public Rgb24 EmulatedColor { get { return Color.ParseHex(((Ratio * cColors[(int)(Foreground ?? 0)] + (ratios.Length - Ratio) * cColors[(int)Background]) / 4).ToString("X8")[2..]); } }
+        public ConsoleColor? Foreground;
+        public ConsoleColor Background;
+        public int Ratio = 0;
+
+        public static ConsolePixelColor[] GenerateColorCombos()
+        {
+            List<ConsolePixelColor> consolePixelColors = new();
+            for (int i = 0; i < cColors.Length; i++)
+            {
+                consolePixelColors.Add(new ConsolePixelColor() { Background = (ConsoleColor)i, Ratio = 0 });
+                for (int j = i + 1; j < cColors.Length; j++)
+                {
+                    for (int k = 1; k < ratios.Length; k++)
+                    {
+                        consolePixelColors.Add(new ConsolePixelColor() { Foreground = (ConsoleColor)j, Background = (ConsoleColor)i, Ratio = k });
+                    }
+                }
+            }
+
+            foreach (var item in consolePixelColors)
+            {
+                Debug.Print($"Emulated color: {item.EmulatedColor:X8}, Foreground color: {item.Foreground}, Background color: {item.Background}, Ratio: {item.Ratio}");
+            }
+
+            return consolePixelColors.ToArray();
+        }
+
+        public static ConsolePixelColor GetClosestColor(Rgb24 Input)
+        {
+            int LowestDistance = int.MaxValue;
+            int Index = 0;
+            for (int i = 0; i < ConsolePixelColorCombos.Length; i++)
+            {
+                int dR = Input.R - ConsolePixelColorCombos[i].EmulatedColor.R;
+                int dG = Input.G - ConsolePixelColorCombos[i].EmulatedColor.G;
+                int dB = Input.B - ConsolePixelColorCombos[i].EmulatedColor.B;
+                int DistanceSq = dR * dR + dG * dG + dB * dB;
+                if (DistanceSq < LowestDistance)
+                {
+                    LowestDistance = DistanceSq;
+                    Index = i;
+                }
+                if (LowestDistance == 0)
+                {
+                    break;
+                }
+            }
+
+            return ConsolePixelColorCombos[Index];
+        }
+    }
+}
--- ConsoleGUI.Drawing/Imaging/ImageConverter.cs
+++ ConsoleGUI.Drawing/Imaging/ImageConverter.cs
@@ -9,50 +9,17 @@ namespace ConsoleGUI.Drawing.Imaging
 {
     public static class ImageConverter
     {
-        static readonly int[] cColors = { 0x000000, 0x000080, 0x008000, 0x008080, 0x800000, 0x800080, 0x808000, 0xC0C0C0, 0x808080, 0x0000FF, 0x00FF00, 0x00FFFF, 0xFF0000, 0xFF00FF, 0xFFFF00, 0xFFFFFF };
-        static readonly Rgb24[] cTable = cColors.Select(x => (Rgb24)Color.ParseHex(x.ToString("X8"))).ToArray();
+        static readonly Rgb24[] cTable = ConsolePixelColor.cColors.Select(x => (Rgb24)Color.ParseHex(x.ToString("X8"))).ToArray();
         public static ConsoleCharacter ColorToConsoleCharacter(Rgb24 color)
         {
-            int Best = int.MaxValue;
-            int RunnerUp = 0;
-
-            Rgb24 BestColor = cTable[0];
-            Rgb24 RunnerUpColor = cTable[1];
-            foreach (Rgb24 Color in cTable)
+            ConsolePixelColor c = ConsolePixelColor.GetClosestColor(color);
+            ConsoleCharacter consoleCharacter = new()
             {
-                int DistanceSq = (Color.R - color.R) ^ 2 + (Color.G - color.G) ^ 2 + (Color.B - color.B) ^ 2;
-                if (DistanceSq < Best)
-                {
-                    RunnerUp = Best;
-                    Best = DistanceSq;
-                    RunnerUpColor = BestColor;
-                    BestColor = Color;
-                }
-                else if (DistanceSq < RunnerUp)
-                {
-                    RunnerUp = DistanceSq;
-                    RunnerUpColor = Color;
-                }
-            }
-
-            char[] ratios = new char[] { (char)0032, (char)9617, (char)9618, (char)9619, (char)9608 }; // 0/4, 1/4, 2/4, 3/4, 4/4
-
-            int index = 0;
-            float smallestDifference = float.MaxValue;
-
-            // Get Ratio'd
-            for (int i = 0; i < ratios.Length; i++)
-            {
-                Rgb24 RatiodColor = new(Convert.ToByte(((4 - i) * BestColor.R + i * RunnerUpColor.R) / 4), Convert.ToByte(((4 - i) * BestColor.G + i * RunnerUpColor.G) / 4), Convert.ToByte(((4 - i) * BestColor.B + i * RunnerUpColor.B) / 4));
-                int DistanceSq = (RatiodColor.R - color.R) ^ 2 + (RatiodColor.G - color.G) ^ 2 + (RatiodColor.B - color.B) ^ 2;
-                if (DistanceSq < smallestDifference)
-                {
-                    smallestDifference = DistanceSq;
-                    index = i;
-                }
-            }
-
-            return new ConsoleCharacter() { Character = ratios[index], ForegroundColor = (ConsoleColor?)Array.IndexOf(cTable, BestColor), BackgroundColor = (ConsoleColor?)Array.IndexOf(cTable, RunnerUpColor) };
+                ForegroundColor = c.Foreground,
+                BackgroundColor = c.Background,
+                Character = ConsolePixelColor.ratios[c.Ratio]
+            };
+            return consoleCharacter;
         }
 
         public static void ImageToBuffer(ref ConsoleCharacter[,] buffer, Image image, WritingSettings settings)
